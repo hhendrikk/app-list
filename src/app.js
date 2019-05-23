744c19876d088e0b5b5ec585165041429a472926
Hendrik Araújo Mourão <hendrik.mourao@autotrac.com.br>
2019-05-23 14:05:07 -0300

    Curso app list - 239

--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
 'use strict'
 
 import React from 'react'
-
 import { hot } from 'react-hot-loader/root'
 
 import 'sass/app.scss'
-import reactLogo from 'images/react-logo.svg'
 
 const App = () => (
-  <div className='container'>
-    <img src={reactLogo} style={{ width: '40px', height: '40px' }} />
-    My App
+  <div>
+    <input type='text' />
+
+    <ul>
+      <li style={{ textDecoration: 'line-through' }}>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+      <li>Item 4</li>
+      <li>Item 5</li>
+    </ul>
+
+    <div>
+      <h3>Mostrar</h3>
+      <a href='#'>Todos</a> | <a href='#'>Finalizados</a> | <a href='#'>A fazer</a>
+    </div>
   </div>
 )
 
